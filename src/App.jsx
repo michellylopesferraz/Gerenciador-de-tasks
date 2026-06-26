9b73a0428973a96dc7f2643cf1da820a228afb11
Michellylopesferraz <michellylopesilva@gmail.com>
2026-06-26 17:02:08 -0300

    Subida Gerenciador de tarefas

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import { v4 as uuidv4 } from "uuid";
+import Title from "./components/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  //TESTE COM API
+  useEffect(() => {
+    async function fetchTasks() {
+      //CHAMANDO A API, limite de 5
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=5",
+      );
+
+      //Convertendo dados para JSON
+      const data = await response.json();
+
+      //ARMAZENAR DADOS NO STATE
+      const formattedTasks = data.map((task) => ({
+        id: task.id,
+        title: task.title,
+        description: "",
+        isCompleted: task.completed,
+      }));
+
+      setTasks(formattedTasks);
+    }
+
+    // fetchTasks();
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //PRECISO ATUALIZAR
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      //NÂO PRECISO ATUALIZAR
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onTaskDelete(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: uuidv4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onTaskDelete={onTaskDelete}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
